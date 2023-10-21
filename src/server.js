f4453e8fc8836058ed9aeecabf348b3b984f19d8
Mu-jun <suo24691@gmail.com>
2023-10-21 23:09:37 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
 
-console.log('hello');
+app.get('/', (req, res) => res.render('home'));
 
-app.listen(3000);
+const listenHandler = () => console.log('Listening on http://localhost:3000');
+
+app.listen(3000, listenHandler);
