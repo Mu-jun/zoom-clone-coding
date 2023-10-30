213e44e72abd4c2e4459be8ce6e72c5b78276281
Mu-jun <suo24691@gmail.com>
2023-10-30 18:56:08 +0900

    첫 접속 시 접속 가능한 Room 보여주기

--- src/server.js
+++ src/server.js
@@ -38,6 +38,8 @@ function countRoomUser(roomName) {
 
 io.on('connection', (socket) => {
   socket['nickname'] = 'Anonymous';
+  console.log(adapter.sids.size); // 첫 접속 시 한 번만 실행한다는 것을 검증하기 위함.
+  socket.emit('rooms_change', getPublicRooms());
   socket.onAny((eventName) => {
     console.log('Socket event :', eventName);
   });
